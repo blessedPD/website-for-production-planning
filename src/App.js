052846a87b2af4d2434780ac5e5eb1b69983b9ad
blessedPD <115356350+blessedPD@users.noreply.github.com>
2022-11-10 16:52:42 +0900

    React: commit again

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import NavBar from "./component/NavBar.js";
 
 
 function App() {
-  
+  const[isLogin, changeIsLogin] = useState(false); //로그인 여부 나타내는 변수
 
   return (
     <div>
       <NavBar />
+      
     </div>
   );
 }
